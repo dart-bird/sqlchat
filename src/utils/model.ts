b6e60292aa2f36a7c0fc0fb082a4dbddd6318376
dart-bird <dartbird.yu@gmail.com>
2024-10-08 03:10:34 +0900

    Add ollama custom model

--- src/utils/model.ts
+++ src/utils/model.ts
@@ -34,7 +34,34 @@ const gpt4ho = {
   cost_per_call: 10,
 };
 
-export const models = [gpt35turbo, gpt4, gpt4turbo, gpt4ho];
+const gpt4homini = {
+  name: "gpt-4o-mini",
+  temperature: 0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+  max_token: 4000,
+  cost_per_call: 1,
+};
+
+const qwen2_5_32b = {
+  name: "qwen2.5:32b",
+  temperature: 0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+  max_token: 10000,
+  cost_per_call: 1,
+};
+
+const qwen2_5_coder_7b = {
+  name: "sammcj/qwen2.5-coder-7b-instruct:q8_0",
+  temperature: 0,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+  max_token: 10000,
+  cost_per_call: 1,
+};
+
+export const models = [gpt35turbo, gpt4, gpt4turbo, gpt4ho, gpt4homini, qwen2_5_32b, qwen2_5_coder_7b];
 
 export const getModel = (name: string) => {
   for (const model of models) {
